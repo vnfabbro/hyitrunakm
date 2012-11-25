16adc67786d211706eeefd9b56c16d65f73ccf64
Veronica Fabbro <vnfabbro@hotmail.com>
2012-11-24 23:17:56 -0300

    Saco la proporciòn y escalo

--- 2012/Script_Final/Script_TodoJunto.m
+++ 2012/Script_Final/Script_TodoJunto.m
@@ -7,6 +7,8 @@ fclose all;
 kernel= func_CargarImagen('kernel.jpg','.\Imagenes');
 
 Img=func_CargarImagen('ImgCompleta.jpg','.\Imagenes');
+MaxOrig = func_ObtenerMaximos2(Img)
+DistanciaOriginal=norm([(MaxOrig(4,1)-MaxOrig(3,1)) (MaxOrig(4,2)-MaxOrig(3,2))])
 
 figure;
 imshow(Img);
@@ -16,7 +18,6 @@ ImgE= imresize(Img,2);
 size(ImgE)
 ImgR = imrotate(ImgE,45);
 
-size(ImgR)
 figure;
 imshow(ImgR);
 
@@ -27,18 +28,24 @@ imshow(MatCorrelacion);
 MatMaximos= func_ObtenerMaximos2(MatCorrelacion)
 
 
-a=norm([(163-122) (96-55)])
-
 angulo= func_ObtenerAngulo(MatMaximos);
 
 mat_rotada = func_RotarImagen(ImgR,angulo );
 
 MatMaximos2= func_ObtenerMaximos2(mat_rotada)
-b=norm([(112-170) (192-190)])
+%saco la distancia de la esquina de arriba a la derecha y de abajo a la
+%derecha
+DistanciaFinal=norm([(MatMaximos2(4,1)-MatMaximos2(3,1)) (MatMaximos2(4,2)-MatMaximos2(3,2))])
 
 figure;
 imshow(mat_rotada);
-prop = b/a
+
+%saco la proporcion y la escalo usando esa proporcion
+prop = DistanciaFinal/DistanciaOriginal
+
+ImgFinal= func_EscalarImagen(mat_rotada,1/prop);
+figure;
+imshow(ImgFinal);
 
 
 
